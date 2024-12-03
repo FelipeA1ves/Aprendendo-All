27b39c1bb286cd6e913b7f6f8bc2019e0bbb51ca
FelipeA1ves <felipealvesbalduino@gmail.com>
2024-12-03 16:55:05 -0300

    Criando projeto com ajuda do chatgpt e aprendendo a usar o git

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include('db.php');
+include('partials/header.php');
+
+$query = "SELECT * FROM posts ORDER BY created_at DESC";
+$result = mysqli_query($conn, $query);
+?>
+
+<h1>Blog</h1>
+<?php while($post = mysqli_fetch_assoc($result)): ?>
+  <div class="post">
+    <h2><a href="post.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+    <p><?php echo substr($post['content'], 0, 100); ?>...</p>
+  </div>
+<?php endwhile; ?>
+
+<?php include('partials/footer.php'); ?>
